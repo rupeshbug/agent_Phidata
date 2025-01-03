d62239960fe3a4a8a15176ed7aea5a8da79237eb
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-03 10:36:10 +0545

    financial agent added

--- financial_agent.py
+++ financial_agent.py
@@ -3,7 +3,7 @@ from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
 
-# web search agent
+## web search agent
 web_search_agent = Agent(
     name="Web Search Agent",
     role="Search the web for the information",
@@ -14,4 +14,12 @@ web_search_agent = Agent(
     markdown=True
 )
 
-
+## financial agent
+finance_agent = Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news=True)],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True
+)
