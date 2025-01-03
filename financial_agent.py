4a2522611eea3df5b615286ee83efb9903e62a85
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-01-03 10:31:10 +0545

    web search agent

--- financial_agent.py
+++ financial_agent.py
@@ -3,3 +3,15 @@ from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
 
+# web search agent
+web_search_agent = Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include the source for the information"]
+    show_tools_calls=True,
+    markdown=True
+)
+
+
